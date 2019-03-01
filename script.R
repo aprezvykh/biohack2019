f690522ccd61dd311cab4328c150c87895813451
aprezvykh <aprezvykh@yandex.ru>
2019-03-02 01:28:50 +0300

    late fuckink night

--- script.R
+++ script.R
@@ -9,14 +9,14 @@ blast <- args[6]
 prefix <- args[7]
 
 bl <- read.table(blast,sep = ",",stringsAsFactors = F)
-names(bl) <- c("query_id", "chr", "q_length", "qstart", "qend", "sstart", "send" ,"seq")
+names(bl) <- c("query_id", "chr", "q_length", "qstart", "qend", "sstart", "send" ,"seq", "mm", "gap")
 
 bl$idx <- seq(1:nrow(bl))
 bl$length <- bl$qend - bl$qstart
 bl$seq <- gsub("-", "", bl$seq)
 
-sink(paste(prefix, "_alignments.fasta", sep = ""))
+sink(paste(prefix, ".alignments.fasta", sep = ""))
 
-cat(paste(">", bl$query_id, "|",bl$chr, ":", bl$sstart, "-",  bl$send, "\n",
+cat(paste(">", bl$query_id, "@",bl$chr, "@", bl$sstart, "@",  bl$send,"@", bl$mm,"@",bl$gap, "\n",
           bl$seq, "\n", sep = ""), sep = "\n")
 sink()
--- script.sh
+++ script.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 genome='../genomes/Aegilops_tauschii.Aet_v4.0.dna.toplevel.fa'
-element='../single_LTR_sabrina_RLG_Atau_Sabrina_B_AF497474-1/seq.fasta'
+element='taushi.fasta'
+
+sec_genome='../genomes/Triticum_urartu.ASM34745v1.dna.toplevel.fa'
+
 
 genome_real=$(realpath $genome)
+sec_genome_real=$(realpath $sec_genome)
 element_real=$(realpath $element)
 
-prefix=$(cat $element_real | head -n 1 | sed "s/>//")
 
-blastn -query $element_real -db $genome_real -num_threads 64 -max_target_seqs 10000000 -outfmt  \
-	 "10 qseqid sseqid qlen qstart qend sstart send sseq" > $prefix.seq
+mkdir single_fa
+samtools faidx $element_real
+trans_ids=$(cat $element_real | grep ">" | awk '{print $1}' | sed "s/>//")
 
-./script.R $element_real.seq $prefix
+for f in $trans_ids;
+	do
+		echo $f
+		samtools faidx $element_real $f > single_fa/$f.fasta
+	done
+
+#rm -r single_fa/
+fasta_list=$(ls single_fa/*.fasta)
+
+for f in $fasta_list;
+	do
+		prefix=$(cat $f | head -n 1 | sed "s/>//")
+		echo "$prefix first blast iteration"
+		blastn -query $f -db $genome_real -num_threads 32 -max_target_seqs 10000000 -outfmt  \
+			 "10 qseqid sseqid qlen qstart qend sstart send sseq mismatch gapopen" > $prefix.seq
+		./script.R $prefix.seq $prefix
+		cp $prefix.firGen.outfmt6 $prefix.alignments.fasta res/
+		echo "$prefix second blast iteration"
+		blastn -query $prefix.alignments.fasta -db $sec_genome_real -num_threads 32  -max_target_seqs 10000000 \
+		-outfmt 6 > $prefix.secGen.outfmt6
+		cp $prefix.secGen.outfmt6 res/
+	done
